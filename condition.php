8af98d14719b6cb8f8b8ec83c8263f7b4e29a80b
celebalane <celebalane@gmail.com>
2017-03-07 17:03:06 +0100

    exo 4

--- condition.php
+++ condition.php
@@ -1,3 +1,21 @@
+<?php
+	$age = 30;
+
+	if ($age>=18){
+		$resultatAge='Vous êtes majeur';
+	}else{
+		$resultatAge='Vous êtes mineur';
+	}
+
+	$isEasy = true;
+
+	if ($isEasy){
+		$resultatEasy= "C'est facile!";
+	}else{
+		$resultatEasy="C'est difficile :(";
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,34 +27,22 @@
 			<article>
 			<h1>Exercice 1</h1>
 				<?php
-					$age = 30;
-
-					if ($age>=18){
-						echo 'Vous êtes majeur';
-					}else{
-						echo 'Vous êtes mineur';
-					}
+					echo $resultatAge;
 				?>
 			</article>
 			<article>
 				<h1>Exercice 2</h1>
 				<?php
-					$isEasy = true;
-
-					if ($isEasy){
-						echo "C'est facile!";
-					}else{
-						echo "C'est difficile :(";
-					}
+					echo $resultatEasy;
 				?>
 			</article>
 			<article>
 				<h1>Exercice 3</h1>
 				<?php
-					$age = 24;
+					$age2 = 24;
 					$genre ='femme';
 
-					if ($age>=18){
+					if ($age2>=18){
 						if($genre == 'homme'){
 							echo 'Vous êtes un homme est vous êtes majeur';
 						}else{
@@ -50,6 +56,42 @@
 						}
 					}
 
+				?>
+			</article>
+			<article>
+				<h1>Exercice 4</h1>
+				<?php
+					$magnitude=1;
+
+					switch ($magnitude) {
+    					case 1:
+        					echo "Micro-séisme impossible à ressentir.";
+        					break;
+    					case 2:
+        					echo "Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.";
+        					break;
+    					case 3:
+        					echo "Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.";
+        					break;
+        				case 4:
+        					echo "Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.";
+        					break;
+        				case 5:
+        					echo "Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.";
+        					break;
+        				case 6:
+        					echo "Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre.";
+        					break;
+        				case 7:
+        					echo "Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.";
+        					break;
+        				case 8:
+        					echo "Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.";
+        					break;
+        				case 9:
+        					echo "Séisme capable de tout détruire sur une très vaste zone.";
+        					break;
+					}
 				?>
 			</article>
 		</section>
