cb2065ef3030b21cc1e89bfcf6ce1b93834501cc
celebalane <celebalane@gmail.com>
2017-03-07 13:57:08 +0100

    exo3

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style type="text/css" src="style/css/style.css"></style>
 	<title>Exercice</title>
 </head>
 <body>
--- variable.php
+++ variable.php
@@ -1,11 +1,20 @@
 <?php
-
+//Exo 1
 $name = 'Emilie<br>';
 echo $name;
 
+//Exo 2
 $nom = 'Leterme';
 $prenom = 'Mango';
 $age = 1;
-echo $nom.' '.$prenom.' '.$age;
+echo $nom.' '.$prenom.' '.$age.'<br>';
+
+//Exo 3
+$kilometre = 1;
+echo $kilometre.'<br>';
+$kilometre = 3;
+echo $kilometre.'<br>';
+$kilometre = 125;
+echo $kilometre.'<br>';
 
 ?>
