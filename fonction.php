4dd89a743db4581b60a9ca0e1477631166043ab2
celebalane <celebalane@gmail.com>
2017-03-08 11:38:08 +0100

    exo 4 fonction

--- fonction.php
+++ fonction.php
@@ -14,6 +14,16 @@ function concatenation($caractere1,$caractere2){
 	return $caractere1.$caractere2;
 }
 
+//Exo 4
+function comparaison($nombre1, $nombre2){
+	if ($nombre1>$nombre2){
+		return 'le premier nombre est le plus grand';
+	}elseif ($nombre1<$nombre2) {
+		return 'le deuxième nombre est le plus grand';
+	}else{
+		return 'les nombres sont égaux';
+	}
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -44,11 +54,17 @@ function concatenation($caractere1,$caractere2){
 		<article>
 		<h1>Exercice 4</h1>
 			<?php
-				
+				echo comparaison(45,67);
 			?>
 		</article>
 		<article>
 		<h1>Exercice 5</h1>
+			<?php
+				
+			?>
+		</article>
+		<article>
+		<h1>Exercice 6</h1>
 			<?php
 				
 			?>
