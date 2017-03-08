6ae03cf244be8b3c92814869b9e347217648a750
celebalane <celebalane@gmail.com>
2017-03-08 16:06:18 +0100

    exo

--- formulaire.php
+++ formulaire.php
@@ -10,24 +10,36 @@
 			<h1>Exercice 1</h1>
 			<form method="get" action="user.php">
 				<label for="nom">Nom</label>
-				<input type="text" name="nom" placeholder="Ex: Dupond" />
+				<input type="text" name="nom" placeholder="Ex: Dupond" id="nom" />
 				<label for="nom">Prénom</label>
-				<input type="text" name="prenom" placeholder="Ex: Jacques" />
+				<input type="text" name="prenom" placeholder="Ex: Jacques" id="prenom" />
 				<button type="submit">Envoi</button>
 			</form>
 		</article>
 		<article>
 			<h1>Exercice 2</h1>
 			<form method="post" action="user.php">
-				<label for="nom">Nom</label>
-				<input type="text" name="nom" placeholder="Ex: Dupond" />
-				<label for="nom">Prénom</label>
-				<input type="text" name="prenom" placeholder="Ex: Jacques" />
+				<label for="nom2">Nom</label>
+				<input type="text" name="nom" placeholder="Ex: Dupond" id="nom2"/>
+				<label for="prenom2">Prénom</label>
+				<input type="text" name="prenom2" placeholder="Ex: Jacques" id="prenom2"/>
 				<button type="submit">Envoi</button>
 			</form>
 		</article>
 		<article>
 			<h1>Exercice 3</h1>
+			<form method="post" action="formulaire.php">
+				<label for="civilite">Civilite</label>
+				<SELECT name="civilite" id="civilite">
+					<option value="madame">Madame</option>
+					<option value="monsieur">Monsieur</option>
+				</SELECT>
+				<label for="nom3">Nom</label>
+				<input type="text" name="nom" placeholder="Ex: Dupond" id="nom3"/>
+				<label for="prenom3">Prénom</label>
+				<input type="text" name="prenom3" placeholder="Ex: Jacques" id="prenom3"/>
+				<button type="submit">Envoi</button>
+			</form>
 			
 		</article>
 		<article>
--- user.php
+++ user.php
@@ -1,22 +1,44 @@
+<?php
+	if(isset($_GET['nom']) &&isset($_GET['prenom'])){
+		$resultat= htmlspecialchars($_GET['nom'].' '.$_GET['prenom']);
+	}else{
+		$resultat = 'il y a une erreur';
+	}
+
+	if(isset($_POST['nom']) &&isset($_POST['prenom'])){
+		$resultat2= htmlspecialchars($_POST['nom'].' '.$_POST['prenom']);
+	}else{
+		$resultat2 = 'il y a une erreur';
+	}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="style/css/style.css">
-	<title>Exercice</title>
+	<title>Exercices</title>
 </head>
 <body>
 	<section>
 		<article>
-			<h1>Exercice 1</h1>
-			<a href="formulaire.php">Retour</a>
-		</article>
-		<article>
-			<h1>Exercice 2</h1>
-			<a href="formulaire.php">Retour</a>
-		</article>
-		<article>
 			<h1>Exercice 3</h1>
-			
+			<?php
+				echo $resultat;
+			?>
+			<a href="formulaire.php"><button type="button">Retour</button></a>
+		</article>
+		<article>
+			<h1>Exercice 4</h1>
+			<?php
+				echo $resultat2;
+			?>
+			<a href="formulaire.php"><button type="button">Retour</button></a>
+		</article>
+		<article>
+			<h1>Exercice 5</h1>
+			<a href="formulaire.php"><button type="button">Retour</button></a>
 		</article>
 		<article>
 			<h1>Exercice 4</h1>
